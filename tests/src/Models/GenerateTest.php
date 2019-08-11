0fdb9446e5272c15ac4a42ee8e50e9d73dde1dc6
LocmanMR <israfilov.ruslan2018@yandex.ru>
2019-08-11 21:08:46 +0300

    '1.1.0'

--- tests/src/Models/GenerateTest.php
+++ tests/src/Models/GenerateTest.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../../../src/Models/Generate.php';
-
 use PHPUnit\Framework\TestCase;
 use Doctrine\DBAL\Connection;
 use App\Models\Generate;
